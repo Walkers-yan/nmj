051df5c4acd0ecb2d3d93783004bde4d60d266cc
tiantian <yanttde@outlook.com>
2020-07-10 14:41:18 +0800

    项目初始化-引入vant组件库

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 导入vant组件
+import Vant from 'vant'
+// 注册vant组件
+import 'vant/lib/index.css'
+
 // 加载全局样式
 import './styles/index.less'
+
+// 加载vant所有组件
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
